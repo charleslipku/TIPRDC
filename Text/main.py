9c1e360fdcb4bc11f1794c1709f4356ff12d06c5
James Duan <34743764+JamesDYX@users.noreply.github.com>
2020-12-27 14:13:24 +0800

    Update main.py

--- Text/main.py
+++ Text/main.py
@@ -151,4 +151,4 @@ def get_classifier(FE_path):
 if __name__ == '__main__':
     # run get_FE or get_ZFE to get a feature extractor constrained by DIM info
     FE = get_FE()
-    CF = get_classifier("Models/extractor/FE_lambda05.pth")
+    CF = get_classifier("Models/extractor/FE.pth")
